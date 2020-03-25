c21b2c276f3b860c749e8f8d45cc5ce26fc5d127
sadawi <hahli144@hotmail.com>
2020-03-25 22:18:09 +0200

    add script

---
+++ append_declarations.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+import sys, re
+
+if (len(sys.argv) != 3):
+	print("Usage: ./append_declarations [input_file] [output_file]")
+try:
+	indentAmount = int(sys.argv[3])
+except:
+	indentAmount = 1
+
+file = open(sys.argv[1], "r")
+inputFile = file.read()
+file.close()
+declarations = re.findall("(?<!;\n\n)(?<=\n\n).+\)", inputFile)
+print(declarations)
+file = open(sys.argv[2], "a")
+for dec in declarations:
+	type, tab, name  = dec.partition("\t")
+	if not dec.startswith("int\tmain"):
+		file.write(type + tab * indentAmount + name + ";\n\n")
+file.close()
