b9334281132bc6a6d7a2b9b3a847f2d633615c60
sadawi <hahli144@hotmail.com>
2020-03-25 22:35:00 +0200

    add error output, remove debug print, fix indentation

--- append_prototypes.py
+++ append_prototypes.py
@@ -2,7 +2,7 @@
 
 import sys, re
 
-if (len(sys.argv) != 3):
+if (len(sys.argv) < 3):
 	print("Usage: append_declarations INPUT_FILE OUTPUT_FILE [INDENTATION]")
 	print("Default indentation is 1.")
 	sys.exit()
@@ -11,12 +11,19 @@ try:
 except:
 	indentAmount = 1
 
-file = open(sys.argv[1], "r")
+try:
+	file = open(sys.argv[1], "r")
+except:
+	print("Input file not found.")
+	sys.exit()
 inputFile = file.read()
 file.close()
 declarations = re.findall("(?<!;\n\n)(?<=\n\n).+\)", inputFile)
-print(declarations)
-file = open(sys.argv[2], "a")
+try:
+	file = open(sys.argv[2], "a")
+except:
+	print("Output file not found.")
+	sys.exit()
 for dec in declarations:
 	type, tab, name  = dec.partition("\t")
 	if not dec.startswith("int\tmain"):
